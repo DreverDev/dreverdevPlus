cb5591214417f5ca01bee2ce810c95db90257350
Dídac Reverte <didacr97@gmail.com>
2023-08-31 00:17:28 +0200

    feat: Add nav logic

--- dreverdevplus/src/components/layout/header/Menu.tsx
+++ dreverdevplus/src/components/layout/header/Menu.tsx
@@ -2,11 +2,13 @@
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { Turn as Hamburger } from 'hamburger-react';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Menu() {
 
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLElement>(null);
+    const buttonRef = useRef<HTMLElement>(null);
 
     const toggleOpen = () => {
         if (isOpen) {
@@ -16,21 +18,35 @@ export default function Menu() {
         }
     }
 
+    const handleClickOutside = (event: MouseEvent) => {
+        if (menuRef.current && buttonRef.current && (!menuRef.current.contains(event.target as Node) && !buttonRef.current.contains(event.target as Node))) {
+            if (isOpen) toggleOpen();
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className="flex text-sm [&>li>a]:font-medium [&>li>a]:text-current [&>li>a]:transition-colors [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2 relative">
-            <Hamburger toggled={isOpen} toggle={setIsOpen} />
-            <nav className={`transform origin-top-right ${isOpen ? 'scale-100' : 'scale-0'} transition-transform duration-300 absolute top-full right-0 w-64 bg-gray-800 text-white p-4 rounded`}>
-                <ul className="flex flex-col gap-y-4">
+            <span className="z-10" ref={buttonRef} onClick={toggleOpen}><Hamburger toggled={isOpen} /></span>
+            <nav ref={menuRef} className={`transform origin-top-right ${isOpen ? 'scale-100' : 'scale-0'} transition-transform duration-300 absolute top-0 right-0 w-64 bg-gray-800 text-white rounded`}>
+                <ul className="flex flex-col py-5">
                     {siteConfig.navItems.map((item) => (
-                        <li key={item.href}>
-                            <Link className="text-xl" href={item.href}>
+                        <Link className="text-xl" href={item.href}>
+                            <li key={item.href} className="px-5 py-3 inline-block relative group">
                                 {item.label}
-                            </Link>
-                        </li>
+                                <span className="absolute bottom-1 left-0 w-0 bg-white opacity-10 transform origin-left group-hover:w-full transition-all duration-300 h-1"></span>
+                            </li>
+                        </Link>
                     ))}
                 </ul>
             </nav>
-            {/* <div id="overlay" className={`fixed top-0 left-0 bg-black h-screen w-screen cursor-pointer z-20 ${isOpen ? 'open' : 'closed'}`} onClick={toggleOpen} /> */}
         </div>
     )
 }
