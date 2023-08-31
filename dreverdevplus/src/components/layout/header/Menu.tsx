bed66658ef4a54e3c026779168e85c78f1195363
Dídac Reverte <didacr97@gmail.com>
2023-08-31 14:53:57 +0200

    fix: Fix nav open close bug

--- dreverdevplus/src/components/layout/header/Menu.tsx
+++ dreverdevplus/src/components/layout/header/Menu.tsx
@@ -7,21 +7,18 @@ import { useEffect, useRef, useState } from "react";
 export default function Menu() {
 
     const [isOpen, setIsOpen] = useState(false);
+
     const menuRef = useRef<HTMLElement>(null);
     const buttonRef = useRef<HTMLElement>(null);
 
     const toggleOpen = () => {
-        if (isOpen) {
-            setIsOpen(false);
-        } else {
-            setIsOpen(true);
-        }
+        setIsOpen(prev => !prev);
     }
 
     const handleClickOutside = (event: MouseEvent) => {
-        if (menuRef.current && buttonRef.current && (!menuRef.current.contains(event.target as Node) && !buttonRef.current.contains(event.target as Node))) {
-            if (isOpen) toggleOpen();
-        }
+        if(!menuRef || !buttonRef) return;
+        if(!menuRef.current?.contains(event.target as Node) && !buttonRef.current?.contains(event.target as Node) && isOpen) toggleOpen();
+        
     }
 
     useEffect(() => {
@@ -30,7 +27,7 @@ export default function Menu() {
         return () => {
             document.removeEventListener("click", handleClickOutside);
         };
-    }, []);
+    }, [isOpen]);
 
     return (
         <div className="flex text-sm [&>li>a]:font-medium [&>li>a]:text-current [&>li>a]:transition-colors [&>li>a]:inline-block [&>li>a]:px-4 [&>li>a]:py-2 relative">
@@ -38,8 +35,8 @@ export default function Menu() {
             <nav ref={menuRef} className={`transform origin-top-right ${isOpen ? 'scale-100' : 'scale-0'} transition-transform duration-300 absolute top-0 right-0 w-64 bg-gray-800 text-white rounded`}>
                 <ul className="flex flex-col py-5">
                     {siteConfig.navItems.map((item) => (
-                        <Link className="text-xl" href={item.href}>
-                            <li key={item.href} className="px-5 py-3 inline-block relative group">
+                        <Link key={item.href} className="text-xl" href={item.href}>
+                            <li className="px-5 py-3 inline-block relative group">
                                 {item.label}
                                 <span className="absolute bottom-1 left-0 w-0 bg-white opacity-10 transform origin-left group-hover:w-full transition-all duration-300 h-1"></span>
                             </li>
